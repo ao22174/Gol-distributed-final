70059b32466917ea9b6034116bf87ccae94b89f9
ao22174 <123552441+ao22174@users.noreply.github.com>
2023-11-28 18:18:22 +0000

    Update distributor.go

--- gol/distributor.go
+++ gol/distributor.go
@@ -22,7 +22,13 @@ type distributorChannels struct {
 }
 
 //This Ticker function is constantly running when a GOL state is being calculated, will only stop once all the turns have finished
-func tickerFunc(p Params, c distributorChannels, client *rpc.Client, done chan bool, request stubs.Request, response *stubs.Response) {
+func tickerFunc(p Params, c distributorChannels, client *rpc.Client, done chan bool, request stubs.Request) {
+	request2 := stubs.Request{
+		Turns:       p.Turns,
+		ImageHeight: p.ImageHeight,
+		ImageWidth:  p.ImageWidth,
+		Threads:     p.Threads,
+	}
 
 	var currentWorld = make([][]byte, p.ImageWidth)
 	for i := range currentWorld {
@@ -35,15 +41,17 @@ loop:
 	for {
 		select {
 		case <-ticker.C:
+			response2 := new(stubs.Response)
+			client.Call(stubs.Retrieve, request2, response2)
+
 			if pause != true {
-				client.Call(stubs.Retrieve, request, response)
 				c.events <- AliveCellsCount{
-					CompletedTurns: response.TurnsCompleted,
-					CellsCount:     response.AliveCount,
+					CompletedTurns: response2.TurnsCompleted,
+					CellsCount:     response2.AliveCount,
 				}
 				for i := 0; i < p.ImageHeight; i++ {
 					for j := 0; j < p.ImageWidth; j++ {
-						readWorld := response.World[i][j]
+						readWorld := response2.World[i][j]
 						currentWorld[i][j] = readWorld
 
 					}
@@ -56,52 +64,59 @@ loop:
 				var outFilename = fmt.Sprintf("%vx%vx%v", p.ImageWidth, p.ImageHeight, p.Turns)
 				c.ioCommand <- ioOutput
 				c.ioFilename <- outFilename
-
+				response2 := new(stubs.Response)
+				client.Call(stubs.Retrieve, request, response2)
 				for i := 0; i < p.ImageHeight; i++ {
 					for j := 0; j < p.ImageWidth; j++ {
-						writeWorld := currentWorld[i][j]
+						writeWorld := response2.World[i][j]
 						c.ioOutput <- writeWorld
 					}
 				}
-				c.events <- StateChange{response.TurnsCompleted, Quitting}
+				c.events <- StateChange{response2.TurnsCompleted, Quitting}
 				done <- true
-				client.Call(stubs.Quit, request, response)
+				client.Call(stubs.Quit, request, response2)
 			case 's':
+				response2 := new(stubs.Response)
+				client.Call(stubs.Retrieve, request, response2)
 				var outFilename = fmt.Sprintf("%vx%vx%v", p.ImageWidth, p.ImageHeight, p.Turns)
 				println(outFilename)
 				c.ioCommand <- ioOutput
 				c.ioFilename <- outFilename
-
 				for i := 0; i < p.ImageHeight; i++ {
 					for j := 0; j < p.ImageWidth; j++ {
-						writeWorld := response.World[i][j]
+						writeWorld := response2.World[i][j]
 						c.ioOutput <- writeWorld
 					}
 				}
 
 			case 'k':
+				response2 := new(stubs.Response)
+				client.Call(stubs.Retrieve, request, response2)
 				var outFilename = fmt.Sprintf("%vx%vx%v", p.ImageWidth, p.ImageHeight, p.Turns)
 				c.ioCommand <- ioOutput
 				c.ioFilename <- outFilename
-
 				for i := 0; i < p.ImageHeight; i++ {
 					for j := 0; j < p.ImageWidth; j++ {
-						writeWorld := currentWorld[i][j]
+						writeWorld := response2.World[i][j]
 						c.ioOutput <- writeWorld
 					}
 				}
-				c.events <- StateChange{response.TurnsCompleted, Quitting}
+				c.events <- StateChange{response2.TurnsCompleted, Quitting}
 				done <- true
-				client.Call(stubs.SuperQuit, request, response)
+				client.Call(stubs.SuperQuit, request, response2)
 
 			case 'p':
+				response2 := new(stubs.Response)
+				client.Call(stubs.Retrieve, request, response2)
+
 				if pause == false {
-					c.events <- StateChange{response.TurnsCompleted, Paused}
-					client.Call(stubs.Pause, request, response)
+					c.events <- StateChange{response2.TurnsCompleted, Paused}
+					client.Call(stubs.Pause, request, response2)
 					pause = true
 				} else if pause == true {
-					c.events <- StateChange{response.TurnsCompleted, Executing}
-					client.Call(stubs.Pause, request, response)
+
+					c.events <- StateChange{response2.TurnsCompleted - 1, Executing}
+					client.Call(stubs.Pause, request, response2)
 					pause = false
 				}
 			}
@@ -140,7 +155,7 @@ func distributor(p Params, c distributorChannels) {
 	request := stubs.Request{World: world, Turns: p.Turns, ImageHeight: p.ImageHeight, ImageWidth: p.ImageWidth, Threads: p.Threads}
 	response := new(stubs.Response)
 
-	go tickerFunc(p, c, client, done, request, response)
+	go tickerFunc(p, c, client, done, request)
 	client.Call(stubs.BrokeOps, request, response)
 
 	turn = response.TurnsCompleted
@@ -156,11 +171,10 @@ func distributor(p Params, c distributorChannels) {
 			c.ioOutput <- writeWorld
 		}
 	}
-	c.events <- ImageOutputComplete{response.TurnsCompleted, outFilename}
-
 	// Make sure that the Io has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle
 	<-c.ioIdle
+	c.events <- ImageOutputComplete{response.TurnsCompleted, outFilename}
 
 	c.events <- StateChange{turn, Quitting}
 	// Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
